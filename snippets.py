c0a9a1e5c579fe570790cd01b1c991f0d14846d4
Leonardo Requena <leoreq@gmail.com>
2022-01-29 18:30:48 -0500

    Added delete and upadate stub

--- snippets.py
+++ snippets.py
@@ -10,7 +10,7 @@ def put(name,snippet):
 
     Returns the name and the snippet
     """
-    logging.error("FIXME: unimplemented - put({!r},{!r}".format(name,snippet))
+    logging.error("FIXME: unimplemented - put({!r},{!r})".format(name,snippet))
 
     return name,snippet
 
@@ -23,3 +23,27 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def delete(name):
+    """
+    Delete the snippet with a given name.
+
+    If there is no such snippet, reutrn '404: Snippet not found'.
+
+    Return 'Snippet Deleted Succesfuly'
+    """
+    logging.error("FIXME: Unimplementer - delete({!r})".format(name))
+    return " "
+
+def update(name,snippet):
+    """
+    Update a snippet with an associated name.
+
+    If there is no such a snippet, then reutrn '404: Snippet not found'.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: unimplemented - update({!r},{!r})".format(name,snippet))
+
+    return name,snippet
+    
